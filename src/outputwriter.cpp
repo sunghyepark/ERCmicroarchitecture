c7b7291da282a93da680c7810361b75050ef21f6
sunghyepark <shpark96@postech.ac.kr>
2021-10-14 11:47:44 +0900

    outputwriter edit

--- src/outputwriter.cpp
+++ src/outputwriter.cpp
@@ -18,23 +18,23 @@ void Qcircuit::QMapper::write_output(Circuit& graph)
 {
     ofstream of(fileName_output);
     //of << "========== output file ==========" << endl;
-    of << "OPEMQASM 2.0" << endl;
+    of << "OPEMQASM 2.0;" << endl;
     of << "include \"qelib1.inc\";" << endl;
     //of << "qreg q[16];" << endl; 
-    //of << "creg q[16];" << endl; 
+    //of << "creg c[16];" << endl; 
     of << "qreg q[" << nqubits << "];" << endl; 
-    of << "creg q[" << nqubits << "];" << endl; 
+    of << "creg c[" << nqubits << "];" << endl; 
     
     //nodeset_out(of, FinalCircuit);
     for (auto g : graph.nodeset)
     {
         if(g.control==-1)
         {
-            of << g.type <<  g.output_type << " " << "q[" << g.target << "]" << endl;
+            of << g.type <<  g.output_type << " " << "q[" << g.target << "];" << endl;
         }
         else
         {
-            of << g.type << " " << "q[" << g.control << "],q[" << g.target << "]" << endl; 
+            of << g.type << " " << "q[" << g.control << "],q[" << g.target << "];" << endl; 
         }
     }
     //cout << " Nodeset out is done" << endl << endl;
